a06c16bb010e427c7191de46c80426748b8da8de
SeungHyunYoo <dhrdlxl@naver.com>
2022-04-03 23:52:31 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (60.62%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        if (nums[right] < target) {
+            return nums.length;
+        }
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        
+        
+        return right;
+    }
+}
