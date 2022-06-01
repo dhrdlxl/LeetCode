14151e89fa5171d0c3b625be6d4e206d05b23696
SeungHyunYoo <dhrdlxl@naver.com>
2022-06-01 21:18:00 +0900

    Time: 2 ms (18.14%), Space: 41.5 MB (25.86%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -6,10 +6,9 @@ public class Solution {
         long num = 1;
         for (int i = 0; i < 32; i++) {
             if ((n & num) > 0) {
-                System.out.print(i + " ");
                 answer++;
             }
-            num= num << 1;
+            num = num << 1;
         }
         
         return answer;
