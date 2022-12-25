bfe5c8aa50fc27ad6856dd81fe0a5e0b3a9cc0e4
SeungHyunYoo <dhrdlxl@naver.com>
2022-12-25 22:45:02 +0900

    Time: 7 ms (86.36%), Space: 54.6 MB (88.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        
+        return false;
+    }
+}
