bf72824c94bdefdc89661223c8ed7b393ee59dbd
SeungHyunYoo <dhrdlxl@naver.com>
2023-06-12 00:39:50 +0900

    Time: 16 ms (88.72%), Space: 48.4 MB (82.98%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,27 @@
+class MyHashSet {
+    private boolean[] hashSet;
+
+    public MyHashSet() {
+        hashSet = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        hashSet[key] = true;
+    }
+    
+    public void remove(int key) {
+        hashSet[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return hashSet[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
