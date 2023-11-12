ef693faa05e842166bcbfe4f87875f946f81b4bc
SeungHyunYoo <dhrdlxl@naver.com>
2023-11-12 22:49:08 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (84.50%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int minOpen = 0;
+        int maxOpen = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (c == ')') {
+                minOpen--;
+                maxOpen--;
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+            if (maxOpen < 0) {
+                return false;
+            }
+            
+            minOpen = Math.max(minOpen, 0);
+        }
+        
+        return minOpen == 0;
+    }
+}
