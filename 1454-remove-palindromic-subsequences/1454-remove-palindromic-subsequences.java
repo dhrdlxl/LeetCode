79824697953c74c7eac854f7952f7b247a2f040f
SeungHyunYoo <dhrdlxl@naver.com>
2025-11-15 19:46:34 +0900

    Time: 0 ms (100%), Space: 42.9 MB (5.56%) - LeetHub

---
+++ 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return 2;
+            }
+            left++;
+            right--;
+        }
+
+        return 1;
+    }
+}
